6e462aeda23f6d770c7ca32e6b09e0b10bc2c516
Dina Kendle <dina.kendle@gmail.com>
2020-05-06 19:04:48 +0000

    Puts more options in CLI, creates local class no. variable,

--- lib/magic_user/api.rb
+++ lib/magic_user/api.rb
@@ -1,8 +1,21 @@
-class Api 
-  
-  
-  ## service file/class responsible for communicating with api.
-  ##going out to it, getting info, and returning it
-  
-  
+class Api
+
+  # def self.get_spells_by_name(spell_name)
+    ##goes out to D&D api
+    ##finds spells by name
+    ##returns that information
+    #what is my endpoint
+    #how do I go there and get what I need?
+    #how do I handle that json and turn it into meaningful data?
+    ######how do I make spell objs from that data
+  # end
+
+  def self.spells_by_klass(klass_number)
+    url = "https://www.dnd5eapi.co/api/spellcasting/#{klass_number}"
+    response = Net::HTTP.get(URI(url))
+binding.pry
+  end
+
 end
+## service file/class responsible for communicating with api.
+##going out to it, getting info, and returning it
--- lib/magic_user/cli.rb
+++ lib/magic_user/cli.rb
@@ -4,6 +4,17 @@ class Cli
     puts " "
     puts "Greetings Magic User, what knowledge do you seek?"
     puts " "
+    puts "Enter your class by number: "
+    puts "1. Bard"
+    puts "2. Cleric"
+    puts "3. Druid"
+    puts "4. Paladin"
+    puts "5. Ranger"
+    puts "6. Sorcerer"
+    puts "7. Warlock"
+    puts "8. Wizard"
+    @klass_number = gets.strip.to_i
+    Api.spells_by_klass(@klass_number)
   end
   ##responsible for handling input FROM user and output TO user
 
