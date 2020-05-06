6319d560636e1eb7268296b81324e198bfaeb0dc
Dina Kendle <dina.kendle@gmail.com>
2020-05-06 18:13:02 +0000

    adds required gems and relative folders in environment.rb

---
+++ lib/environment.rb
@@ -0,0 +1,10 @@
+
+
+require "pry"
+require "httparty" ##interacts with my api
+require "json"##parses api data to json
+#require "http" ##accidentaly installed this...see if we can use it
+
+require_relative "./magic_user/api"
+require_relative "./magic_user/cli"
+require_relative "./magic_user/spell"
--- lib/magic_user/cli.rb
+++ lib/magic_user/cli.rb
@@ -1,5 +1,10 @@
-class Cli 
-  
+class Cli
+
+  def run
+    puts " "
+    puts "Greetings Magic User, what knowledge do you seek?"
+    puts " "
+  end
   ##responsible for handling input FROM user and output TO user
-  
+
 end
