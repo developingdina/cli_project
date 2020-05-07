86aab5fa8077f56e819baaf69394cf97a58f8626
Dina Kendle <dina.kendle@gmail.com>
2020-05-07 21:37:50 +0000

    Adds a new print category method shell to Cli class

--- lib/magic_user/cli.rb
+++ lib/magic_user/cli.rb
@@ -7,9 +7,22 @@ class Cli
     puts "Enter a spell to know it's power:"
     @spell_name = gets.strip.downcase
     Api.spells_by_name(@spell_name)
-    puts "What other knowledge do you seek?"
-    puts "~Enter another spell or exit to close the Tome~"
+    print_category(Spell.all)
+  end
+
+  def print_category(spells)
+    puts " "
+    puts "You start to feel a mystical energy surround you."
+    # puts "Unfamiliar symbols flash through your mind and a faint voice calls out"
+    # puts "...#{@spell_name}..."
+    puts "Choose a number to further master this spell:"
+    spells.each.with_index(1) do |spell, index|
+      puts "#{index}. #{spell.range}"
+
+
+    end
   end
   ##responsible for handling input FROM user and output TO user
-
+##puts "What other knowledge do you seek?"
+##puts "~Enter another spell or exit to close the Tome~"
 end
