7b5b565786cfe225c73f35e59a1ef40c0227dd88
Dina Kendle <dina.kendle@gmail.com>
2020-05-07 16:16:19 +0000

    Adds setter/getter and initialize method to Spell class

--- lib/magic_user/api.rb
+++ lib/magic_user/api.rb
@@ -1,41 +1,42 @@
 class Api
 
-  # def self.get_spells_by_name(spell_name)
-    ##goes out to D&D api
-    ##finds spells by name
-    ##returns that information
-    #what is my endpoint
-    #how do I go there and get what I need?
-    #how do I handle that json and turn it into meaningful data?
-    ######how do I make spell objs from that data
-  # end
-
   def self.spells_by_name(spell_name)
-    url = "http://www.dnd5eapi.co/api/spells/#{spell_name}"
-    response = Net::HTTP.get(URI(url))
 
-    names = JSON.parse(response)["name"]
-    ##name of spell
-    descriptions = JSON.parse(response)["desc"]
-    ##spell descriptions
-    higher_lvl_desc = JSON.parse(response)["higher_level"]
-    ##descriptions at a higher level of user
-    ranges = JSON.parse(response)["range"]
-    ##how long or wide it is cast
-    components = JSON.parse(response)["components"]
-    ##things needed to case: verbal, somatic, or materials
-    materials = JSON.parse(response)["material"]
-    ##type of material needed to cast a spell
-    durations = JSON.parse(response)["duration"]
-    ##how long the spell lasts
-    casting_times = JSON.parse(response)["casting_time"]
-    ##how long the spell lasts
-    levels_to_use = JSON.parse(response)["level"]
-    ##level needed in magic to cast
-    klasses = JSON.parse(response)["classes"]
-    ##classes that can use the spell
-binding.pry
+    url = "https://www.dnd5eapi.co/api/spells/#{spell_name}"
+    response = Net::HTTP.get(URI(url))
+    spell_info = JSON.parse(response)
+
+    spell_info.each do |spell_details|
+      Spell.new(name)
+    end
+        binding.pry
   end
+#   def self.spells_by_name(spell_name)
+#     url = "http://www.dnd5eapi.co/api/spells/#{spell_name}"
+#     response = Net::HTTP.get(URI(url))
+#
+#     names = JSON.parse(response)["name"]
+#     ##name of spell
+#     descriptions = JSON.parse(response)["desc"]
+#     ##spell descriptions
+#     higher_lvl_desc = JSON.parse(response)["higher_level"]
+#     ##descriptions at a higher level of user
+#     ranges = JSON.parse(response)["range"]
+#     ##how long or wide it is cast
+#     components = JSON.parse(response)["components"]
+#     ##things needed to case: verbal, somatic, or materials
+#     materials = JSON.parse(response)["material"]
+#     ##type of material needed to cast a spell
+#     durations = JSON.parse(response)["duration"]
+#     ##how long the spell lasts
+#     casting_times = JSON.parse(response)["casting_time"]
+#     ##how long the spell lasts
+#     levels_to_use = JSON.parse(response)["level"]
+#     ##level needed in magic to cast
+#     klasses = JSON.parse(response)["classes"]
+#     ##classes that can use the spell
+# binding.pry
+#   end
 
 end
 ## service file/class responsible for communicating with api.
--- lib/magic_user/spell.rb
+++ lib/magic_user/spell.rb
@@ -1,5 +1,20 @@
 class Spell
-  
+  attr_accessor :name, :description, :range
+
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
   ## responsible for creating spell obj's
   ##
 end
