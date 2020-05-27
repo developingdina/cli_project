4ce7c7e069bfa3d6066d62a99ff6268b0ab3086e
Dina Kendle <dina.kendle@gmail.com>
2020-05-27 14:52:50 +0000

    Updates rake gem

--- Rakefile
+++ Rakefile
@@ -1,3 +1,2 @@
 require "bundler/gem_tasks"
 task :default => :spec
-spec.add_dependency "rake", ">= 12.3.3"
--- cli_project/lib/environment.rb
+++ cli_project/lib/environment.rb
@@ -1,10 +1,9 @@
 
 
 require "pry"
-require "httparty" ##interacts with my api
-require "json"##parses api data to json
-#require "http" ##accidentaly installed this...see if we can use it
-
+require "httparty"
+require "json"
+require "rake", ">= 12.3.3"
 
 require_relative "./magic_user/api"
 require_relative "./magic_user/cli"
