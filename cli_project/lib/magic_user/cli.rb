e3398071e403507994d37c16864bcb65352afe86
DKendle <dina.kendle@gmail.com>
2022-05-13 16:28:48 -0400

    adds ascii art for user experience

--- cli_project/lib/magic_user/cli.rb
+++ cli_project/lib/magic_user/cli.rb
@@ -1,10 +1,30 @@
-class Cli
-
+class Cli  
   def run
-    puts " "
-    puts "Welcome to the Tome of Spells:"
-    puts " "
-    puts "Is it Magic you seek: y or n"
+    puts "   
+    :?JJJJJ?^  .?J7  .JJ7.~J7.  ~JJ:  ^7JYJ?^ .?JJJJJ~   ~?YYJ7:  ^J?:  :JJ~ ^?YYYJ.
+      .PB5^~JBG~ PBY  .GB7 ^BBBY:^BG :PBY~^^!!  YBG!!!~ ^GBJ^^~5B5..GGB5^.GB::BB?~~7.
+       5BJ   5BG.5BY  .GB7 ^BP75BPGG.5BP   ^!7. JGG5Y5. GB5    .GBJ.GG7YBGGG: !J5P5J^
+       PB5..!GG7 YBP. ~GB~ ^BP  ^YGG.!BG~. 7BB. YBP^^^^.!BG!...JBG:.GB. :JGB:.?:.:5BG
+    :55555Y!:  .!YP55J^  !55:   :?  :?5PPP5Y..J55555Y  .75PPPY~  ^55~   .?: 55555J:            
+           .75J?5?.  ::..::..   .....      ....      ..:..      .::.    ..    .::.  .::.        
+           ?G5. !BY  .?BGP55PY~ :GGYJGP^   !BGG^   ^YGPYY55  :YGP55PP7. JBP7. JB5 7GPJJ5J       
+           ^YYJJ?5:    5Y~  ^GB7.GG!:GG!  ^GP?GG: ~BG~  ..^.^BB!   .5BP 7BGGG?YBY ?GP?!~:       
+          .JP?PGY5.  ~!JJJ~..GB?.GG5GB5. :GB5!GGP.!BG:  ^GG~^BG^    JBG 7BY !GBGY ::^!YGG^      
+         :JJY!~?^Y?.7?~PG5JYGP? .BB~.?GG7PBY777PB5:?GGJ7JBB! !PGY??PGY: JB5   ~P5 JP?!JBG^      
+         ?BGG. : ~Y5Y..~^~~^:   :^^:  .^^^^.   :^^:  :~!~^:.   :^~~^.  .^^^.    : .^~!~:        
+         ?BGP:   !PJPP?Y.~P.                                                                    
+         .?5GBPPGP! ?5BBGPJ.                                                                    
+            .^~^.     :!!:"
+    puts "                                   ---------------------------------"
+    puts "                                 ||                                 |"
+    puts "                                 ||                                 |"
+    puts "                                 ||Welcome to the Tome of Spells:   |"
+    puts "                                 ||                                 |"
+    puts "                                 ||                                 |"
+    puts "                                 ||                                 |"
+    puts "                                 ||                                 |"
+    puts "                                   --------------------------------- "
+    puts "                                    Is it Magic you seek: Y or N"
     @input = gets.strip.downcase
     prompt_user if orig_input(@input)
     prompt_input = gets.strip.downcase
---
+++ cli_project/lib/magic_user/logos.rb
@@ -0,0 +1,22 @@
+class Logos
+
+
+    def dungeons_and_dragons
+        puts "   
+      :?JJJJJ?^  .?J7  .JJ7.~J7.  ~JJ:  ^7JYJ?^ .?JJJJJ~   ~?YYJ7:  ^J?:  :JJ~ ^?YYYJ.
+        .PB5^~JBG~ PBY  .GB7 ^BBBY:^BG :PBY~^^!!  YBG!!!~ ^GBJ^^~5B5..GGB5^.GB::BB?~~7.
+         5BJ   5BG.5BY  .GB7 ^BP75BPGG.5BP   ^!7. JGG5Y5. GB5    .GBJ.GG7YBGGG: !J5P5J^
+         PB5..!GG7 YBP. ~GB~ ^BP  ^YGG.!BG~. 7BB. YBP^^^^.!BG!...JBG:.GB. :JGB:.?:.:5BG
+      :55555Y!:  .!YP55J^  !55:   :?  :?5PPP5Y..J55555Y  .75PPPY~  ^55~   .?: 55555J:            
+             .75J?5?.  ::..::..   .....      ....      ..:..      .::.    ..    .::.  .::.        
+             ?G5. !BY  .?BGP55PY~ :GGYJGP^   !BGG^   ^YGPYY55  :YGP55PP7. JBP7. JB5 7GPJJ5J       
+             ^YYJJ?5:    5Y~  ^GB7.GG!:GG!  ^GP?GG: ~BG~  ..^.^BB!   .5BP 7BGGG?YBY ?GP?!~:       
+            .JP?PGY5.  ~!JJJ~..GB?.GG5GB5. :GB5!GGP.!BG:  ^GG~^BG^    JBG 7BY !GBGY ::^!YGG^      
+           :JJY!~?^Y?.7?~PG5JYGP? .BB~.?GG7PBY777PB5:?GGJ7JBB! !PGY??PGY: JB5   ~P5 JP?!JBG^      
+           ?BGG. : ~Y5Y..~^~~^:   :^^:  .^^^^.   :^^:  :~!~^:.   :^~~^.  .^^^.    : .^~!~:        
+           ?BGP:   !PJPP?Y.~P.                                                                    
+           .?5GBPPGP! ?5BBGPJ.                                                                    
+              .^~^.     :!!:"
+    end
+
+end
